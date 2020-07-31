34bdf61631781aaebfd85f93fc248f40c2b4d3c9
Gabriel Meireles <cunhacruzmeireles@gmail.com>
2020-07-31 02:57:11 -0300

    Update fim da quarta aula

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -37,7 +37,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://maisumflix.herokuapp.com/categorias';
 
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
